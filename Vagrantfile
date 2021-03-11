ab734e96ea4b1f34e60fa0212db3d884dfaadd3f
Chris Mague <github@mague.com>
2021-03-10 16:41:43 -0800

    update ubuntu version

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "ubuntu/groovy64"
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.synced_folder ".", "/mnt/host_machine"
   config.vm.provider :virtualbox do |vb|
--- provision.sh
+++ provision.sh
@@ -16,19 +16,19 @@ sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/config.xml /var/lib/jenkins/
 sudo mkdir -p /var/lib/jenkins/users/admin
 sudo cp $VAGRANT_HOST_DIR/JenkinsConfig/users/admin/config.xml /var/lib/jenkins/users/admin/
 sudo chown -R jenkins:jenkins /var/lib/jenkins/users/
+sudo sh -c 'echo "jenkins ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/jenkins'
+
 
 ########################
 # Docker
 ########################
 echo "Installing Docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get -y install docker.io
 sudo systemctl enable docker
 sudo usermod -aG docker ${USER}
 sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
+sudo usermod -aG docker vagrant
 
 ########################
 # nginx
